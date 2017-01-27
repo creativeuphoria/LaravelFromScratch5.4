dc9ba7cf1b2ea50a619361f925ad821a7cdbb06f
creativEuphoria <me@creativeuphoria.co.uk>
2017-01-27 23:37:10 +0000

    Basic routing and views (Episode 2)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaravelFromScratch
                 </div>
 
                 <div class="links">
